13fd3da0f6655c1a6affbf830cc543d2618ab173
NatePena <Nathaniel.pena1@marist.edu>
2023-02-13 17:57:54 -0500

    Logger, main and tester
    Made changes to logger file and added main and test logger

---
+++ Lab_Goose/logger.py
@@ -0,0 +1,26 @@
+# import your libraries here.
+from datetime import datetime 
+# the name of the log file to write to.
+
+
+log_file = "log-file_"+datetime.today().strftime('%Y-%m-%d')+".log"
+
+def log(text, log_file=log_file):
+    # open up the log file in the correct mode.
+    # create a string that has the current date and time in the beginning of the text.
+    # Ensure the string ends with a new line character.
+    # append the text to the end of the file.
+    # close the file.
+    return None
+
+
+def dump(log_file=log_file):
+    '''
+    This function prints out each line in the log file to the console
+    '''
+    # open up the log file in the correct mode.
+    # read the file into a list.
+    # iterate through each item in the list and print out the results.
+    # close the file.
+    return None
+print(log_file)
